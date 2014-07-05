3af3090c314123c6fd37b8dbe9ebbe956301ece9
Celebes <kris1337@gmail.com>
2014-07-05 23:02:04 +0100

    Poprawka w skrypcie

--- JEKYLL_LOCAL.sh
+++ JEKYLL_LOCAL.sh
@@ -8,6 +8,6 @@ bundle exec jekyll serve --detach
 echo Uruchomiono Jekyll lokalnie!
 
 # Uruchom przeglądarkę i przejdź pod adres, gdzie działa lokalny JEKYLL
-start http://localhost:4000
+xdg-open http://localhost:4000
 
 echo Uruchomiono przegladarke pod adresem localhost:4000!
